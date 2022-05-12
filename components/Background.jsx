c8fbdb272a98108128e7788cb9ccfe508edb4e40
jmslasso <juanmsolisprado@gmail.com>
2022-05-12 03:20:23 -0500

    IMÁGENES

--- components/Background.jsx
+++ components/Background.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 const Background = styled.div`
-    background-image: url("https://media2.giphy.com/media/bLdgTj2jCKe9Wf94Km/giphy.gif?cid=790b761192907dde8c2fe4ec7b44a80ed02bd2cf6b146f2d&rid=giphy.gif&ct=g");
+    background-image: url("./images/giphy.gif");
+    //background-image: url("https://media2.giphy.com/media/bLdgTj2jCKe9Wf94Km/giphy.gif?cid=790b761192907dde8c2fe4ec7b44a80ed02bd2cf6b146f2d&rid=giphy.gif&ct=g");
     background-repeat: no-repeat;
     background-size: cover;
     width: 100%;
